785b0a1fc93a53a90caca50ff058a9ca3c5c4d0c
jshin8 <joshshin8@gmail.com>
2015-10-23 11:18:33 -0700

    ready for heroku deploy attempt #1

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/my_heroku_app");
+mongoose.connect( process.env.MONGOLAB_URI ||
+                  process.env.MONGOHQ_URL || 
+                  "YOUR OWN LOCAL URL HERE" );
 
+//mongoose.connect("mongodb://localhost/my_heroku_app");
 // After creating a new model, require and export it:
 // module.exports.Tweet = require("./tweet.js");
--- server.js
+++ server.js
@@ -17,7 +17,5 @@ app.get('/', function(req, res) {
   res.render("index");
 });
 
-app.listen(3000, function() {
-  console.log("express-heroku-starter is running on port 3000");
-});
+app.listen(process.env.PORT || 3000);
 
