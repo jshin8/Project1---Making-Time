0592b2e0f5684a7893a2cb74db1730373c664075
jshin8 <joshshin8@gmail.com>
2015-10-28 17:29:52 -0700

    added sessions and cookies and is now working!

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var db = require("./models/index");
 var User = require("./models/user");
+var cookieParser = require('cookie-parser');
 var session = require('express-session');
 
 //create express app object
@@ -14,6 +15,9 @@ var app = express();
 //middleware
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+// must use cookieParser before session
+// configure app to use cookieParser
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //middleware for cookies
@@ -21,7 +25,7 @@ app.use(session({
  saveUninitialized: true,
  resave: true,
  secret: 'SuperSecretCookie',
- cookie: { maxAge: 60000 }
+ cookie: { maxAge: 600000 }
 }));
 
 // middleware to manage sessions
@@ -48,32 +52,33 @@ app.use('/', function (req, res, next) {
   next();
 });
 
+//get static index
+app.get('/', function (req, res) {
+  res.render("index");
+});
+
 // profile page
 app.get('/profile', function (req, res) {
   // check for current (logged-in) user
   // console.log("works");
-  // req.currentUser(function (err, user) {
+   req.currentUser(function (err, user) {
   //   // show profile if logged-in user
   //   console.log("yes");
-  //   if (user) {
-  //     res.render('profile');           
+     if (user) {
+       res.render('profile');           
   //   // redirect if no user logged in
-  //   } else {
-      res.render('profile');
-  //   }
-  // });
+     } else {
+      res.render('index');
+     }
+   });
 });
 
-app.get('/', function(req, res) {
-  res.render("index");
-});
-
-
+// signup route
 app.get('/signup', function (req, res) {
     req.currentUser(function (err, user) {
    //  // redirect if current user
      if (user) {
-       res.render('index');
+       res.redirect('profile');
      } else {
       res.render('signup');
      }
@@ -94,14 +99,21 @@ app.post('/users', function (req, res) {
 
 // login route (renders login view)
 app.get('/login', function (req, res) {
-  res.render('login');
+  req.currentUser(function (err, user) {
+   //  // redirect if current user
+     if (user) {
+       res.redirect('profile');
+     } else {
+      res.render('login');
+     }
+   });
 });
 
 // authenticate user and set session
 app.post('/login', function (req, res) {
   var userData = req.body.user;
   User.authenticate(userData.username, userData.password, function (err, user) {
-    //req.login(user);
+    req.login(user);
     //redirect to user profile
     res.redirect('/profile');
   });
